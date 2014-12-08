c8c5713a41046940a3e9e79b199df37e4c467e86
touchnan <88052350@qq.com>
2014-12-08 11:58:14 +0800

    just change

---
+++ touchin/src/test/java/touchin/V.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package touchin;
+
+import cn.touch.kit.encrypt.MD5;
+
+/**
+ * Oct 17, 2013
+ * @author <a href="mailto:touchnan@gmail.com">chegnqiang.han</a>
+ *
+ */
+public class V {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        
+        
+    }
+
+}
